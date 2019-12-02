2fd2d6d7b6d6f7e8a3103f4d96c9b37f70e327e2
Patrick <patrickpop96@icloud.cmo>
2019-12-02 22:43:29 +0200

    Current_time program with external library(Joda)

---
+++ external_library/src/ShowTime.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class ShowTime {
+ public static void main(String[] args){
+     LocalTime currentTime= LocalTime.now();
+     System.out.println("Time is: "+currentTime);
+ }
+}
